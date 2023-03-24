b8159a0956e4f53fd55ff2299dcacd481b751a0f
Sempain <trollnacho.np@gmail.com>
2023-03-24 13:32:31 +0100

    ctor hehco

--- Pelicula.java
+++ Pelicula.java
@@ -5,9 +5,18 @@ public class Pelicula {
     private String nombre;
     private int duracion;
     private double valoracion;
+    private genero Tipo;
     private enum genero{
         terror,accion,romance,comedia,suspense,aventura,drama,animacion,infantil,policiaca,misterio,historico,familiar,seasonal;
     }
+
+    public Pelicula(String nombre,int duracion,double valoracion,genero Genero){
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.valoracion = valoracion;
+        this.Tipo = Genero;
+    }
+
     private String Comentarios[];
 
     public String getNombre() {
