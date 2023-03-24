3797abd2243d59fc8e26ad9bc0e430d8711e96e6
Sempain <trollnacho.np@gmail.com>
2023-03-24 12:49:43 +0100

    Atributos

---
+++ Pelicula.java
@@ -0,0 +1,12 @@
+/**
+ * Pelicula
+ */
+public class Pelicula {
+    private String nombre;
+    private int duracion;
+    private double valoracion;
+    private enum genero{
+        terror,accion,romance,comedia,suspense,aventura,drama,animacion,infantil,policiaca,misterio,historico,familiar,seasonal;
+    }
+    private String Comentarios[];
+}
