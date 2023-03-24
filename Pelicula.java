412714c12143e5c845706c715b0f932c1e7dffcb
Sempain <trollnacho.np@gmail.com>
2023-03-24 13:16:39 +0100

    Setters getters (falta el array de comentarios)

--- Pelicula.java
+++ Pelicula.java
@@ -9,4 +9,28 @@ public class Pelicula {
         terror,accion,romance,comedia,suspense,aventura,drama,animacion,infantil,policiaca,misterio,historico,familiar,seasonal;
     }
     private String Comentario[];
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getDuracion() {
+        return duracion;
+    }
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+    public double getValoracion() {
+        return valoracion;
+    }
+    public void setValoracion(double valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    
+
+
+    
 }
