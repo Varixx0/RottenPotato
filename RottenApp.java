51cac990b3ce6c58249840f69ab670696d188b3f
pokB01 <pokB01@x.y.z>
2023-03-27 08:52:38 +0200

    Arrays y pruebas

--- RottenApp.java
+++ RottenApp.java
@@ -1,18 +1,28 @@
-
+import java.util.ArrayList;
 
 public class RottenApp {
+    private static ArrayList <Pelicula> Peliculas = new ArrayList<>();
+    private static ArrayList <Usuarios> Usuarios = new ArrayList<>();
     public static void main(String[] args) {
+        ContenidoInicial();
+
         
     }
     public static void ContenidoInicial() {
-        Usuarios Manuel = new Usuarios("Manuel" , "1234" , true);
-        Usuarios Ana = new Usuarios("Ana" , "HolaSoyAna" , false);
-        Usuarios Duncan= new Usuarios("Duncan" , "InLoveWithSofi" , true);
-        Usuarios Sergi = new Usuarios("Marra" , "GafasDeJeff" , false);
-        Pelicula ComoEntrenarATuDragon = new Pelicula("Como entrenar a tu dragon", 98, 5, Pelicula.genero.infantil);
-        Pelicula AmericanHistoryX = new Pelicula("American History X", 120, 4.1, Pelicula.genero.drama);
-        Pelicula ElSilencioDeLosCorderos = new Pelicula("El silencio de los corderos", 120, 4.3, Pelicula.genero.terror);
-        Pelicula IndianaJones = new Pelicula("Indiana Jones: En busca del arca perdida", 115, 3.5, Pelicula.genero.aventura);
-        Pelicula Frozen = new Pelicula("Frozen", 108, 5.1, Pelicula.genero.animacion);
+        Usuarios.add(new Usuarios("Pepe","1234",true));
+        Usuarios.add(new Usuarios("Ana" , "HolaSoyAna" , false));
+        Usuarios.add(new Usuarios("Duncan" , "InLoveWithSofi" , true));
+        Usuarios.add(new Usuarios("Marra" , "GafasDeJeff" , false));
+        Peliculas.add(new Pelicula("Como entrenar a tu dragon", 98, 5, Pelicula.genero.infantil));
+        Peliculas.add(new Pelicula("American History X", 120, 4.1, Pelicula.genero.drama));
+        Peliculas.add(new Pelicula("Indiana Jones: En busca del arca perdida", 115, 3.5, Pelicula.genero.aventura));
+        Peliculas.add(new Pelicula("Frozen", 108, 5.1, Pelicula.genero.animacion));
+        Peliculas.add(new Pelicula("El silencio de los corderos", 120, 4.3, Pelicula.genero.terror));
+
+        
+    }
+
+    public static void CrearPelicula(){
+        
     }
 }
