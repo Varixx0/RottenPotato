51d3e3051352851319053c806532f07f0cb08615
pokB01 <pokB01@x.y.z>
2023-03-27 10:29:03 +0200

    Métodos de comprobación

--- Comprobacio.java
+++ Comprobacio.java
@@ -1,12 +1,40 @@
+
 public class Comprobacio {
     
-    public boolean comprobarValoracion(int posi){
+    public boolean comprobarValoracion(int index){
         boolean bien = false;
+        if (RottenApp.Peliculas.get(index).getValoracion() > 0 && RottenApp.Peliculas.get(index).getValoracion() <= 5 ) {
+            bien = true;
+        }
+        return bien;
+    }
 
-        if (RottenApp.Peliculas.get(posi).getValoracion() ) {
-            
+    public boolean comprobarNombre(int index,String nombre){
+        boolean bien = false;
+        if (RottenApp.Peliculas.get(index).getNombre().toLowerCase().equals(nombre.toLowerCase())) {
+            bien = false;
+        }
+        else{
+            bien = true;
         }
 
         return bien;
+    }
+
+    public boolean comprobarDuracion(int index){
+        boolean bien = false;
+        if (RottenApp.Peliculas.get(index).getDuracion() >= 60) {
+            bien = true;
+        }
+        return bien;
+    }
+
+    public boolean comprobarTodo(int index,String nombre){
+        boolean bien = false;
+        if (comprobarDuracion(index) && comprobarNombre(index, nombre) &&comprobarValoracion(index)){
+            bien = true;
+        }
+        return bien;
+    
     }
 }
