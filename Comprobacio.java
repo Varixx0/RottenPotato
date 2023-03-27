d39657db3f85546bf310aac804d6ad13f4e161f9
pokB01 <pokB01@x.y.z>
2023-03-27 10:41:13 +0200

    Funcionalidades de comprobació

--- Comprobacio.java
+++ Comprobacio.java
@@ -1,7 +1,7 @@
 
 public class Comprobacio {
     
-    public boolean comprobarValoracion(int index){
+    public static boolean comprobarValoracion(int index){
         boolean bien = false;
         if (RottenApp.Peliculas.get(index).getValoracion() > 0 && RottenApp.Peliculas.get(index).getValoracion() <= 5 ) {
             bien = true;
@@ -9,19 +9,20 @@ public class Comprobacio {
         return bien;
     }
 
-    public boolean comprobarNombre(int index,String nombre){
+    public static boolean comprobarNombre(String nombre){
         boolean bien = false;
-        if (RottenApp.Peliculas.get(index).getNombre().toLowerCase().equals(nombre.toLowerCase())) {
-            bien = false;
+        for (int i = 0; i < RottenApp.Peliculas.size(); i++) { 
+            if (RottenApp.Peliculas.get(i).getNombre().equals(nombre)) {
+                bien = false;
+            }
+            else{
+                bien = true;
+            }   
         }
-        else{
-            bien = true;
-        }
-
         return bien;
     }
 
-    public boolean comprobarDuracion(int index){
+    public static boolean comprobarDuracion(int index){
         boolean bien = false;
         if (RottenApp.Peliculas.get(index).getDuracion() >= 60) {
             bien = true;
@@ -29,9 +30,9 @@ public class Comprobacio {
         return bien;
     }
 
-    public boolean comprobarTodo(int index,String nombre){
+    public static boolean comprobarTodo(int index,String nombre){
         boolean bien = false;
-        if (comprobarDuracion(index) && comprobarNombre(index, nombre) &&comprobarValoracion(index)){
+        if (comprobarDuracion(index) && comprobarNombre(nombre) &&comprobarValoracion(index)){
             bien = true;
         }
         return bien;
